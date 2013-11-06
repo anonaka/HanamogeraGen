a7a32baa4631cd99c6332579244478a9119187ca
Akira Nonaka <nonaka@mac.com>
2013-11-06 22:18:05 +0900

    under development.

--- src/ver2/hanamogera2.rb
+++ src/ver2/hanamogera2.rb
@@ -6,6 +6,12 @@ require 'pp'
 load File.join(File.dirname(__FILE__),'table-100nin-2.rb')
 
 class Hanamogera2
+    
+  def initialize(order)
+    # Markov chainの階数
+    @order = order
+  end
+  
   def pickup_random(ar)
     return ar[rand(ar.length-1)]
   end
@@ -20,23 +26,8 @@ class Hanamogera2
     return result
   end
   
-  def gen_mid(tbl,phrase,len,limit)
-    count = limit - phrase.length
-    count.times { 
-      puts phrase
-      key = phrase[(-len)..(-1)]      
-      candidates = find_connection(tbl,key,len)
-      if candidates != []
-        phrase += pickup_random(candidates)[0][-1]
-      else
-        puts "no candidates"
-        phrase += "あ"
-      end
-    }
-    puts phrase
-  end
-
   def gen_mid_recur(tbl,phrase,len,limit)
+    puts phrase
     key = phrase[(-len)..(-1)]      
     candidates = find_connection(tbl,key,len)
     candidates.each { |c|
@@ -52,20 +43,36 @@ class Hanamogera2
     }
     return nil
   end
+
+  def add_end(tbl,level,phrase,len)
+    puts phrase
+    key = phrase[(-len)..(-1)]      
+    candidates = find_connection(tbl,key,len)
+    if candidates != []
+      phrase += pickup_random(candidates)[0][-1]
+      return phrase
+    else
+      return nil
+    end
+  end
+
+  def generate(level,length)
+    len = length - 1
+    hanamogera = pickup_random($tbl_begin_3_a)[0]  
+    hanamogera = gen_mid_recur($tbl_mid_3_a,hanamogera,level-1,len)
+    if hanamogera == nil
+      return "fail"
+    end
+    hanamogera = add_end($tbl_end_3_a,level,hanamogera,len)
+    return hanamogera
+  end
 end
 
 if __FILE__ == $0
   level = 3
   len = level-1
   ha = Hanamogera2.new
-  limit = 5
-  result = nil
-  while result == nil
-    hanamogera = ha.pickup_random($tbl_begin_3_a)[0]  
-    result = ha.gen_mid_recur($tbl_mid_3_a,hanamogera,len,limit)
-    if result == nil
-      #puts "failed"
-    end
-  end
+  limit = 10
+  result = ha.generate(level,limit)
   puts result
 end
